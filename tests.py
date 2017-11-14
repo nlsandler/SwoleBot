de16521a3340c564d32d606f5f512684967d0d4c
Nora Sandler <nlsandler@gmail.com>
2017-11-14 16:03:16 -0500

    added command line arg to control interval time, and tests for reminder time logic

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 import unittest
 from unittest.mock import patch
+import datetime
 import swolebot
 
 DEFAULT_USER_NAME = "bob"
@@ -13,6 +14,8 @@ INSPIRATION_COMMAND = 'inspiration'
 FORM_COMMAND = 'form'
 
 class SwoleBotTest(unittest.TestCase):
+
+
     def setUp(self):
         super().setUp()
         key_word = SWOLEBOT
@@ -88,6 +91,38 @@ class SwoleBotTest(unittest.TestCase):
         self.bot.send_private_message.assert_called_with(DEFAULT_EMAIL, swolebot.RESPONSES["leave_success"])
         #verify they're in the subscriber list
         self.assertEqual(self.bot.subscribers, [])
+
+    
+    def test_is_reminder_time_day_false(self):
+        fake_date = datetime.datetime(2017, 11, 12, 12) # A Saturday
+        with patch('datetime.datetime') as mocked_datetime:
+            mocked_datetime.now.return_value = fake_date
+            is_time = self.bot.is_reminder_time()
+            self.assertFalse(is_time)
+
+    def test_is_reminder_time_hour_false(self):
+        fake_date = datetime.datetime(2017, 11, 14, 20) # A Monday at 8pm
+        with patch('datetime.datetime') as mocked_datetime:
+            mocked_datetime.now.return_value = fake_date
+            is_time = self.bot.is_reminder_time()
+            self.assertFalse(is_time)   
+
+    def test_is_reminder_time_true(self):
+        fake_date = datetime.datetime(2017, 11, 14, 12) # A Monday at 12pm
+        with patch('datetime.datetime') as mocked_datetime:
+            mocked_datetime.now.return_value = fake_date
+            is_time = self.bot.is_reminder_time()
+            self.assertTrue(is_time)  
+
+
+    def test_time_last_reminded_false(self):
+        fake_time_reminded = datetime.datetime(2017, 11, 14, 12)
+        fake_current_date = datetime.datetime(2017, 11, 14, 12, 30)
+        self.bot.time_last_reminded = fake_time_reminded
+        with patch('datetime.datetime') as mocked_datetime:
+            mocked_datetime.now.return_value = fake_current_date
+            is_time = self.bot.is_reminder_time()
+            self.assertFalse(is_time)  
 
 if __name__ == "__main__":
     unittest.main()
